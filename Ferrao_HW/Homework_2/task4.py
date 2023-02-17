3854dd6a3fad2ebeac779d9e5d031b57af204bc4
ivaniukihornik <ivaniukihornik@gmail.com>
2023-02-17 10:32:20 +0200

    added task 4 of HW2

---
+++ Ferrao_HW/Homework_2/task4.py
@@ -0,0 +1,10 @@
+# 4
+# You have two groups of people. One group consists of omnivores, the other only vegetarians.
+# An omnivore is a vegetarian but a vegetarian is not an omnivore.
+# Display a list of guests who can eat vegetables and herbs.
+
+omnivores = ['Alex Cool', 'Danny Man', 'Jenny Fire', 'Lennox Grey']
+vegetarians = ['Leroy Green', 'Lemon Grass', 'Lisa Corn']
+
+veg_herb_eaters = omnivores + vegetarians
+print(veg_herb_eaters)
