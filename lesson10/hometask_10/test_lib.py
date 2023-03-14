5b51e0a15361951cffa46d56a3b802da5d0eeba8
ivaniukihornik <ivaniukihornik@gmail.com>
2023-03-14 23:24:07 +0200

    updated hometask 10

--- lesson10/hometask_10/test_lib.py
+++ lesson10/hometask_10/test_lib.py
@@ -2,13 +2,16 @@ from lesson10.hometask_10 import data_lib
 
 
 def test_default_string_type():
+    """Check if default value of random_string_generation function is in string type"""
     assert type(data_lib.random_string_generation()) == str
 
 
 def test_custom_string_type():
+    """Check if custom value of random_string_generation function is in string type"""
     assert type(data_lib.random_string_generation(data_lib.digit)) == str
 
 
 def test_random_string_type():
+    """Check if random value of random_string_generation function is in string type"""
     assert type(data_lib.random_string_generation(data_lib.get_random_number())) == str
 
